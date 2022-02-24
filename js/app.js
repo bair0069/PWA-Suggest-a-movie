0d69a601f70738c6fd712cbe368fb4c9cee0bdca
bair0069 <bair0069@algonquinlive.com>
2022-02-24 00:30:58 -0500

    Results stored in DB and accessible offline

--- js/app.js
+++ js/app.js
@@ -6,12 +6,8 @@ const APP = {
   init: () => {
     //- - Register SW
     APP.registerSW();
+    APP.getConfig()
     //- - Create Search DB
-    APP.createSearchDB();
-    //- - Execute Page specific code
-    APP.pageSpecific();
-    //- - ADD Listeners
-    APP.addListeners();
   },
   //TODO: remove commented lines from SW
   //REGISTER SW
@@ -20,16 +16,17 @@ const APP = {
     //if the worker is registered
     if ("serviceWorker" in navigator) {
       console.log("pretending to add service worker");
-      // navigator.serviceWorker.register('../sw.js').catch(function (err) {
-      //   // Something went wrong during registration. The sw.js file
-      //   // might be unavailable or contain a syntax error.
-      //   console.warn(err);
-      // });
-      // navigator.serviceWorker.ready.then((registration) => {
-      //   // .ready will never reject... just wait indefinitely
-      //   APP.SW = registration.active;
-      //   //save the reference to use later or use .ready again
-      // });
+      navigator.serviceWorker.register("../sw.js").catch(function (err) {
+        // Something went wrong during registration. The sw.js file
+        // might be unavailable or contain a syntax error.
+        console.warn(err);
+      });
+      navigator.serviceWorker.ready.then((registration) => {
+        // .ready will never reject... just wait indefinitely
+        APP.createSearchDB();
+        APP.SW = registration.active;
+        //save the reference to use later or use .ready again
+      });
     }
   },
   //CREATE SEARCH DB
@@ -52,7 +49,7 @@ const APP = {
 
       // store searches under their search keyword in searchStore
       let searchOptions = {
-        keyPath: 'keyword',
+        keyPath: "keyword",
         autoIncrement: false,
       };
 
@@ -62,16 +59,15 @@ const APP = {
         keyPath: "id",
         autoIncrement: false,
       };
-      APP.DB.createObjectStore(
-        "suggestStore",
-        suggestOptions
-      );
+      APP.DB.createObjectStore("suggestStore", suggestOptions);
     };
     dbOpenRequest.onerror = (err) => {
       console.warn(err.message);
     };
     dbOpenRequest.onsuccess = (ev) => {
       APP.DB = dbOpenRequest.result;
+      //- - Execute Page specific code
+      APP.pageSpecific();
       console.log(APP.DB.name, "ready to be used");
     };
   },
@@ -82,17 +78,20 @@ const APP = {
   //- -TODO: fourohfour page
   pageSpecific: () => {
     if (document.body.id === "home") {
-      //TODO:add listeners to previousSearches
+      //get previousSearches
+      APP.getPrevSearches()
     }
     if (document.body.id === "results") {
-      APP.getResultsFromDB()
+      APP.getResultsFromDB();
       //TODO:add listeners to results cards
     }
     if (document.body.id === "suggest") {
     }
     if (document.body.id === "fourohfour") {
-      //TODO: add listeners to previousSearches
+      //get previousSearches
+      APP.getPrevSearches()
     }
+    APP.addListeners();
   },
   //TODO:ADD LISTENERS
   addListeners: () => {
@@ -141,23 +140,26 @@ const APP = {
     let search = document.getElementById("inputSearch");
     search = search.value;
     ev.preventDefault();
-    console.log(APP.checkForSearchMatches(search))
+    console.log(search);
     //check db for matches
+    if (search.length) {
+      APP.checkForSearchMatches(search);
+    }
     //if (match):
     //navigate to results
-    // await APP.navigate('search',search)
-    //TODO:pull results from DB using url keyword
-      // - DONE in pageSpecific
-    //TODO: display results
-      // -DONE in displayCards
+    //DONE in checkForSearchMatches
+    //pull results from DB using url keyword
+    // - DONE in pageSpecific
+    //display results
+    // -DONE in displayCards
     //Else If doesn't match:
-    //TODO:fetch results
-    await APP.fetchResults(search);
-    //TODO:store results needs to be passed results and keyword
+    //fetch results
+    // DONE in checkForSearchMatches
+    //store results needs to be passed results and keyword
     // this is done in the fetch
-    //TODO:navigate to results page
-    // await APP.navigate('search',search)
-    //TODO:display results
+    //navigate to results page
+    //done in the addResultsToDB function
+    //display results
   },
   //TODO:CHECK FOR SEARCH MATCHES
   //- -TODO: make matches return result not just true or false
@@ -166,15 +168,20 @@ const APP = {
     let tx = APP.createTransaction("searchStore", "readonly");
     console.log({ tx });
     let searchStore = tx.objectStore("searchStore");
-    let request = searchStore.get(search)
+    console.log(search);
+    let request = searchStore.get(search);
+    console.log(request);
     request.onerror = (err) => {
       console.warn(err);
     };
-    request.oncomplete = (ev) => {
-      APP.results = ev.target.result
-      console.log(APP.results)
+    request.onsuccess = (ev) => {
+      console.log(ev.target);
+      if (ev.target.result == undefined) {
+        APP.fetchResults(search);
+      } else {
+        APP.navigate("search", search);
+      }
     };
-    return 'this is working',APP.results
   },
   //- - create transaction
   createTransaction: (storeName, mode) => {
@@ -193,21 +200,42 @@ const APP = {
   },
   //- -TODO: return results if match exists
   tmdbAPIKEY: "fd746aee539e0204da54b3425652e549",
-  
+  url: `https://api.themoviedb.org/3/configuration?api_key=fd746aee539e0204da54b3425652e549`,
   tmdbBASEURL: "https://api.themoviedb.org/3/",
   //FETCH
+  getConfig: () => {
+    fetch(APP.url)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        APP.tmdbIMAGEBASEURL = data.images.secure_base_url;
+        APP.tmdbCONFIGDATA = data.images;
+      })
+      .catch((err) => {
+        {
+          console.log(err);
+        }
+      });
+  },
+
   fetchResults: (search) => {
     //check if online
+    //if not online navigate to 404 page
     if (APP.isOnline) {
       let url = `https://api.themoviedb.org/3/search/movie?api_key=${APP.tmdbAPIKEY}&language=en-US&query=${search}&page=1&include_adult=false`;
       fetch(url)
         .then((response) => response.json())
         .then((data) => {
           APP.addResultsToDB(data.results, search);
+          console.log(data.results);
         })
         .catch((err) => console.warn(`Fetch failed due to: ${err.message}`));
-      // APP.addResultsToDB is the callback
     }
+    else{
+      APP.navigate('offline')
+    }
+    
   },
   //ADD RESULTS TO DB
   addResultsToDB: (results, keyword) => {
@@ -217,52 +245,92 @@ const APP = {
     let addRequest = searchStore.add(results);
     addRequest.onsuccess = (ev) => {
       console.log("success");
+      APP.navigate("search", keyword);
     };
     addRequest.onerror = (err) => {
       console.warn("Failed to add", err.target.error);
     };
   },
   //NAVIGATE
-  navigate:(searchType,keyword,id,title)=>{
+  navigate: (searchType, keyword, id, title) => {
     //build custom url for suggest or search
-    let url =""
-    if (searchType=='search') {
-      url=`http://127.0.0.1:5500/results.html?keyword=${keyword}` 
+    let url = "";
+    if (searchType == "search") {
+      url = `http://127.0.0.1:5500/results.html?keyword=${keyword}`;
+      APP.getResultsFromDB();
+    } else if (searchType == "suggest") {
+      url = `http://127.0.0.1:5500/suggest.html?id=${id}&title=${title}`;
     }
-    else if (searchType=='suggest'){
-      url=`http://127.0.0.1:5500/suggest.html?id=${id}&title=${title}`
+    else if (searchType=="offline"){
+      url=`http://127.0.0.1:5500/404.html`
     }
 
-    window.location = url
-
+    window.location = url;
   },
   tmdbIMAGEBASEURL: null,
   tmdbCONFIGDATA: null,
-  //TODO: GET RESULTS
-  getResultsFromDB:()=>{
-    let url = new URL(window.location.href)
-    let params = url.searchParams
-    let keyword = `"${params.get('keyword')}"`
-    console.log(keyword)
-    APP.checkForSearchMatches(keyword)
+  //GET RESULTS
+  getResultsFromDB: () => {
+    let url = new URL(window.location.href);
+    let params = url.searchParams;
+    let keyword = `${params.get("keyword")}`;
+    let results = null;
+    console.log(keyword);
+    let tx = APP.createTransaction("searchStore", "readonly");
 
+    let searchStore = tx.objectStore("searchStore");
+    console.log(searchStore);
+
+    let request = searchStore.get(keyword);
+    console.log(request);
+
+    request.onerror = (err) => {
+      console.warn(err);
+    };
+    request.onsuccess = (ev) => {
+      results = ev.target.result;
+      APP.displayCards(results,keyword);
+    };
   },
-  //TODO:DISPLAY
-  displayCards:()=>{
-    
-    // create transaction
-    // get results from database
-    // display results
+  //DISPLAY
+  displayCards: (results,keyword) => {
+    let titleArea = document.querySelector(".titleArea");
+    titleArea.innerHTML = `<p class="resultsMessage"> Showing results for  "${keyword}" </p>`;
+
+    //display all the movie cards based on the results array
+    //in APP.results
+    //these results could be from the database or from a fetch
+    console.log(results);
+    let resultsCards = document.getElementById('resultsCards')
+    let df = document.createDocumentFragment()
+    resultsCards.textContent="";
+    console.log('clearing')
+    console.log(`APP.tmdbIMAGEBASEURL}w185${results[0].poster_path}`)
+    resultsCards.setAttribute("class", "resultsCards");
+    results.forEach((item) => {
+      let img = document.createElement("img")
+      if(item.poster_path){img.src=`${APP.tmdbIMAGEBASEURL}w185${item.poster_path}`}
+      else{img.src="../img/noimage.png"}
+      img.setAttribute('alt',`A movie poster of ${item.title}`)
+      let li = document.createElement("li");
+      li.setAttribute("class", "card")
+      li.setAttribute('data-id',item.id);
+      li.innerHTML = `<h2>${item.title}</h2><div class="description"><p>${item.overview}</p></div> <h3 class="suggested"> <a data-id=${item.id}>Suggested Movies</a> </h3>`;
+      li.innerHTML = `<h2>${item.title}</h2>`;
+      li.prepend(img)
+      df.append(li);
+    });
+    resultsCards.append(df)
   },
   //TODO:CLICK ON CARD
   //TODO:GET SUGGESTED MOVIES
 
   //TODO:STORE SUGGESTED MOVIES
-  addSuggestToDB: (id,results) => {
+  addSuggestToDB: (id, results) => {
     console.log("adding results to db");
     results.id = id;
     let tx = APP.createTransaction("suggestStore", "readwrite");
-    console.log(results)
+    console.log(results);
     let searchStore = tx.objectStore("suggestStore");
 
     let addRequest = searchStore.add(results);
@@ -274,12 +342,42 @@ const APP = {
     };
   },
   //TODO:SHOW SUGGESTED MOVIES
+  getPrevSearches:()=>{
+    let tx = APP.createTransaction('searchStore','readonly')
+
+    let searchStore = tx.objectStore('searchStore')
+
+    let request = searchStore.getAllKeys()
+
+    request.onerror = (err)=>{console.warn(err)}
+    request.onsuccess = (ev)=>{
+      APP.createPrevSearchesList(ev.target.result)
+    }
+  },
+  createPrevSearchesList:(result)=>{
+    let searches = document.querySelector('.previousSearches')
+    searches.addEventListener('click',(ev)=>{
+      let keyword = ev.target.closest('a').textContent
+      APP.navigate('search',keyword)
+    })
+    let df = document.createDocumentFragment()
+    result.forEach((item)=>{
+      let li = document.createElement('li')
+      li.innerHTML=`<a class="prevSearchItem">${item}</a>`
+      df.append(li)
+    })
+    searches.append(df)
+
+  },
   deferredPrompt: null,
   //TODO:INSTALL PROMPT
   //TODO:ONLINE/OFFLINE
-  changeStatus: (ev) => {
-    console.log(ev);
-  },
+  changeStatus: (status) => {
+    if (status='online'){
+    document.querySelector('.offline').style.display="none"
+    }
+    else{document.querySelector('.offline').style.display="inline-block"}
+},
   //TODO:LIMIT CACHE
 };
 
